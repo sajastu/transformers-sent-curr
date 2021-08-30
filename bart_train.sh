b49c2295c71161d841eb5f77c50f92934c04505d
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-08-30 13:39:35 -0400

    init

--- bart_train.sh
+++ bart_train.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-CUDA_VISIBLE_DEVICES=0 python examples/pytorch/summarization/run_summarization.py --task_mode abstractive \
 python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summarization/run_summarization.py \
     --task_mode exttractive \
     --model_name_or_path facebook/bart-large \
@@ -26,24 +25,24 @@ python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summariza
 
 CUDA_VISIBLE_DEVICES=0 python examples/pytorch/summarization/run_summarization.py  \
 
-python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summarization/run_summarization.py \
-    --task_mode abstractive \
-    --model_name_or_path facebook/bart-large \
-    --do_train \
-    --do_eval \
-    --do_predict \
-    --output_dir /disk1/sajad/bart-outputs/test \
-    --per_device_train_batch_size=1 \
-    --per_device_eval_batch_size=8  \
-    --learning_rate 3e-5 \
-    --weight_decay 0.01 \
-    --adam_beta2 0.98 \
-    --num_train_epochs 5 \
-    --overwrite_output_dir \
-    --evaluation_strategy steps --eval_steps 100 --save_steps 100 --warmup_steps 100 --logging_steps 100 \
-    --text_column document \
-    --summary_column summary \
-    --train_file /home/sajad/transformers/reddit_tifu/train.json \
-    --validation_file /home/sajad/transformers/reddit_tifu/validation.json \
-    --test_file /home/sajad/transformers/reddit_tifu/validation.json \
-    --predict_with_generate
+#python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summarization/run_summarization.py \
+#    --task_mode abstractive \
+#    --model_name_or_path facebook/bart-large \
+#    --do_train \
+#    --do_eval \
+#    --do_predict \
+#    --output_dir /disk1/sajad/bart-outputs/test \
+#    --per_device_train_batch_size=1 \
+#    --per_device_eval_batch_size=8  \
+#    --learning_rate 3e-5 \
+#    --weight_decay 0.01 \
+#    --adam_beta2 0.98 \
+#    --num_train_epochs 5 \
+#    --overwrite_output_dir \
+#    --evaluation_strategy steps --eval_steps 100 --save_steps 100 --warmup_steps 100 --logging_steps 100 \
+#    --text_column document \
+#    --summary_column summary \
+#    --train_file /home/sajad/transformers/reddit_tifu/train.json \
+#    --validation_file /home/sajad/transformers/reddit_tifu/validation.json \
+#    --test_file /home/sajad/transformers/reddit_tifu/validation.json \
+#    --predict_with_generate
