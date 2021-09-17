a36ea89d37989efcdf896944034fce01c281e83f
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-09-17 00:18:53 -0400

    init

--- bart_eval.sh
+++ bart_eval.sh
@@ -9,12 +9,12 @@ export SAVE_MODEL_DIR=/home/code-base/user_space
 #  --model_name_or_path $SAVE_MODEL_DIR/saved_models/$MODEL/$M_ID/checkpoint-20000/\
 
 python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summarization/run_summarization.py --task_mode abstractive\
-    --model_name_or_path /disk1/sajad/bart-outputs/currSumm/checkpoint-20000/ \
+    --model_name_or_path /trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/saved_models/bart-ext/bart-curr/checkpoint-20000 \
     --do_predict \
     --train_file $DS_BASE_DIR_P/train.json \
     --validation_file $DS_BASE_DIR_P/validation.json \
     --test_file $DS_BASE_DIR_P/test.json \
-    --output_dir /disk1/sajad/bart-outputs/currSumm/checkpoint-20000/ \
+    --output_dir /trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/saved_models/bart-ext/bart-curr/checkpoint-20000/ \
     --per_device_train_batch_size=2 \
     --per_device_eval_batch_size=8  \
     --overwrite_output_dir \
