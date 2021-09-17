77fad3ce45914ab16bd5577fd0b0e867d88ec8ed
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-09-16 23:58:37 -0400

    init

--- bart_eval.sh
+++ bart_eval.sh
@@ -1,20 +1,20 @@
 
 export DS_BASE_DIR_P=/tmp/transformers/reddit_tifu_enhanced-ext-wRg/
-export DS_BASE_DIR_P=/home/user_space/transformers/reddit_tifu_enhanced-ext-wRg/
+export DS_BASE_DIR_P=reddit_tifu/
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MODEL=bart-ext
 export M_ID=reddit-lr3e5-sentDecoder-superloss-lambda0.9
 export SAVE_MODEL_DIR=/home/code-base/user_space
+#  --model_name_or_path $SAVE_MODEL_DIR/saved_models/$MODEL/$M_ID/checkpoint-20000/\
 
-
-python -m torch.distributed.launch --nproc_per_node=8 examples/pytorch/summarization/run_summarization.py --task_mode abstractive\
-    --model_name_or_path $SAVE_MODEL_DIR/saved_models/$MODEL/$M_ID/checkpoint-24000/\
+python -m torch.distributed.launch --nproc_per_node=2 examples/pytorch/summarization/run_summarization.py --task_mode abstractive\
+    --model_name_or_path /disk1/sajad/bart-outputs/currSumm/checkpoint-20000/ \
     --do_predict \
     --train_file $DS_BASE_DIR_P/train.json \
     --validation_file $DS_BASE_DIR_P/validation.json \
     --test_file $DS_BASE_DIR_P/test.json \
-    --output_dir /$SAVE_MODEL_DIR/saved_models/$MODEL/$M_ID/checkpoint-24000/ \
+    --output_dir /disk1/sajad/bart-outputs/currSumm/checkpoint-20000/ \
     --per_device_train_batch_size=2 \
     --per_device_eval_batch_size=8  \
     --overwrite_output_dir \
---
+++ post_stats/results_stats.py
@@ -0,0 +1,67 @@
+import json
+
+from somajo import SoMaJo
+from tqdm import tqdm
+
+tokenizer = SoMaJo("de_CMC", split_camel_case=True)
+
+class Result():
+    def __init__(self, path='', test=''):
+        self.path = path
+
+        if len(test) == 0:
+            self.summaries = self.read_summaries(self.path)
+
+        else:
+            self.summaries = self.read_test_summaries(test)
+
+        self.stat = {
+            'avg_tokens': 0,
+            'avg_sents': 0
+        }
+
+
+    def read_test_summaries(self, test_path):
+        out = []
+        with open(test_path) as fR:
+            for l in fR:
+               out.append(json.loads(l.strip())['summary'])
+        return out
+
+    def read_summaries(self, path):
+        out = []
+        with open(path) as fR:
+            for l in fR:
+                out.append(l.strip())
+        return out
+
+    def _tokenize(self, text):
+        sent = []
+        tokens = []
+
+        sentences = tokenizer.tokenize_text([text])
+        for sentence in sentences:
+            for token in sentence:
+                # print("{}\t{}\t{}".format(token.text, tokens.token_class, tokens.extra_info))
+                tokens.append(token.text)
+            # print()
+
+        return {'tokens': tokens, 'sents': sent}
+
+    def compute_tokens(self):
+        for j, s in tqdm(enumerate(self.summaries), total=len(self.summaries)):
+
+            ent_stats = self._tokenize(s)
+
+            self.stat['avg_tokens'] += len(ent_stats['tokens'])
+            self.stat['avg_sents'] += len(ent_stats['sents'])
+
+        for key, val in self.stat.items():
+            self.stat[key] = (float(val) / int((j+1)))
+
+        print(self.stat)
+
+if __name__ == '__main__':
+    # r = Result('generated_predictions_lambda9ch24.txt')
+    r = Result(test='../reddit_tifu/test.json')
+    r.compute_tokens()
