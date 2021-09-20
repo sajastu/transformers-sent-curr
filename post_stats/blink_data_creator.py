5c39d7e8444d5e8af0365b2dac2f62108df6a7a8
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-09-20 12:49:03 -0400

    init

--- post_stats/blink_data_creator.py
+++ post_stats/blink_data_creator.py
@@ -1,3 +1,96 @@
+# import glob
+# import json
+# import os
+# import re
+#
+# from somajo import SoMaJo
+# import spacy
+#
+# nlp = spacy.load("en_core_web_lg")
+# nlp.disable_pipe("parser")
+# nlp.enable_pipe("senter")
+# tokenizer = SoMaJo("en_PTB")
+#
+#
+# def sentencizer(text):
+#     sents = []
+#     doc = nlp(text)
+#     sent_lst = doc.sents
+#     for sent in sent_lst:
+#         sents.append(sent.text)
+#     return sents
+#
+#
+# cases = []
+# src_tkns = []
+# iter = 0
+#
+# for f in glob.glob("blink/*.txt"):
+#     str = ''
+#     token_counter = 0
+#     size = os.path.getsize(f)
+#
+#     with open(f) as fR:
+#
+#         for line_num, l in enumerate(fR):
+#             size -= len(l)
+#             if len(l.strip()) > 0 :
+#                 str += l.strip()
+#                 summary_sents = sentencizer(str)
+#                 src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
+#                 token_count = sum(sum(1 for t in s) for s in src_sentences_tkns)
+#
+#                 # if token_count > 350 or l.strip() == fR.readlines()[-1].strip():
+#                 if not size:
+#                     print('yohoo last line')
+#                 if token_count > 300 or not size:
+#                     src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
+#
+#                     # should store
+#                     for j, sentence in enumerate(src_sentences_tkns):
+#                         sent_tkns = []
+#                         for token in sentence:
+#                             sent_tkns.append(token.text.lower())
+#                         src_tkns.append(sent_tkns)
+#
+#                     ent = {
+#                         'id': f + f'-{iter}',
+#                         'document': '</s><s> '.join([' '.join(s) for s in src_tkns]),
+#                         'summary': 'This is the gold',
+#                         'ext_labels': [0 for s in range(len(src_tkns))],
+#                         'rg_labels': [0 for s in range(len(src_tkns))]
+#                     }
+#                     cases.append(ent)
+#                     counter = 0
+#                     iter += 1
+#                     src_tkns = []
+#                     str = ''
+#
+#                 else:
+#                     str += ' '
+#                     src_tkns = []
+#
+#                     continue
+#
+#
+#     # summary_sents = sentencizer(str)
+#     # src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
+#     # sent_num = sum(1 for _ in src_sentences_tkns)
+#     # src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
+#
+#
+# if not os.path.exists('../blink_test_segmented/'):
+#     os.makedirs('../blink_test_segmented/')
+#
+# with open('../blink_test_segmented/test_300.json', mode='w') as fW:
+#     for e in cases:
+#         json.dump(e, fW)
+#         fW.write('\n')
+#
+#
+#
+##############################################################################################
+
 import glob
 import json
 import os
@@ -21,6 +114,7 @@ def sentencizer(text):
     return sents
 
 
+bart_cases = []
 cases = []
 src_tkns = []
 iter = 0
@@ -35,17 +129,16 @@ for f in glob.glob("blink/*.txt"):
         for line_num, l in enumerate(fR):
             size -= len(l)
             if len(l.strip()) > 0 :
-                str += l.strip()
+                str = l.strip()
                 summary_sents = sentencizer(str)
                 src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
                 token_count = sum(sum(1 for t in s) for s in src_sentences_tkns)
 
-                # if token_count > 350 or l.strip() == fR.readlines()[-1].strip():
-                if not size:
-                    print('yohoo last line')
-                if token_count > 300 or not size:
-                    src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
+                if token_count > 50:
 
+
+                # if token_count > 350 or l.strip() == fR.readlines()[-1].strip():
+                    src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
                     # should store
                     for j, sentence in enumerate(src_sentences_tkns):
                         sent_tkns = []
@@ -53,25 +146,25 @@ for f in glob.glob("blink/*.txt"):
                             sent_tkns.append(token.text.lower())
                         src_tkns.append(sent_tkns)
 
-                    ent = {
-                        'id': f + f'-{iter}',
-                        'document': '</s><s> '.join([' '.join(s) for s in src_tkns]),
-                        'summary': 'This is the gold',
-                        'ext_labels': [0 for s in range(len(src_tkns))],
-                        'rg_labels': [0 for s in range(len(src_tkns))]
-                    }
-                    cases.append(ent)
-                    counter = 0
-                    iter += 1
-                    src_tkns = []
-                    str = ''
-
-                else:
-                    str += ' '
+                        ent = {
+                            'id': f + f'-{iter}',
+                            'document': '</s><s> '.join([' '.join(s) for s in src_tkns]),
+                            'summary': 'This is the gold',
+                            'ext_labels': [0 for s in range(len(src_tkns))],
+                            'rg_labels': [0 for s in range(len(src_tkns))]
+                        }
+                    bart_cases.append(ent)
                     src_tkns = []
 
-                    continue
-
+                ent = {
+                    'id': f + f'-{iter}',
+                    'document': '</s><s> '.join([' '.join(s) for s in src_tkns]),
+                    'summary': 'This is the gold',
+                    'ext_labels': [0 for s in range(len(src_tkns))],
+                    'rg_labels': [0 for s in range(len(src_tkns))]
+                }
+                cases.append(ent)
+                iter += 1
 
     # summary_sents = sentencizer(str)
     # src_sentences_tkns = tokenizer.tokenize_text(summary_sents)
@@ -82,11 +175,15 @@ for f in glob.glob("blink/*.txt"):
 if not os.path.exists('../blink_test_segmented/'):
     os.makedirs('../blink_test_segmented/')
 
-with open('../blink_test_segmented/test_300.json', mode='w') as fW:
-    for e in cases:
+with open('../blink_test_segmented/test_50_m1_bart.json', mode='w') as fW:
+    for e in bart_cases:
         json.dump(e, fW)
         fW.write('\n')
 
+with open('../blink_test_segmented/test_50_m1_all.json', mode='w') as fW:
+    for e in cases:
+        json.dump(e, fW)
+        fW.write('\n')
 
 
 
--- post_stats/integrate_blink_preds.py
+++ post_stats/integrate_blink_preds.py
@@ -1,23 +1,86 @@
+# import json
+# import subprocess
+#
+# segmented_preds = []
+# with open('/trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/saved_models/bart-ext/bart-curr/generated_predictions.txt') as fR:
+#     for l in fR:
+#         segmented_preds.append(l.strip())
+#
+#
+# METHOD = '300'
+# segmented_ids = []
+# paragraphs = []
+# with open(f'blink_test_segmented/test_{METHOD}.json') as fR:
+#     for l in fR:
+#         segmented_ids.append(json.loads(l.strip())['id'])
+#         paragraphs.append(json.loads(l.strip())['document'])
+#
+# cases = {}
+# cases_par = {}
+# for seg_id, par, seg_pred in zip(segmented_ids, paragraphs, segmented_preds):
+#     case_id = seg_id.split('-')[0]
+#     if case_id not in cases.keys():
+#         cases[case_id] = seg_pred
+#         cases_par[case_id] = [{'segment_id': seg_id, 'paragraph_text': par, 'paragraph_summary': seg_pred}]
+#     else:
+#         cases[case_id] += ' '
+#         cases[case_id] += seg_pred
+#         cases_par[case_id].append({'segment_id': seg_id, 'paragraph_text': par, 'paragraph_summary': seg_pred})
+#
+#
+# with open(f'blink_test_segmented/final_preds_{METHOD}.json', mode='w') as fW:
+#     for k, v in cases.items():
+#         json.dump(
+#             {'file': k,
+#              'summary': v
+#              },
+#             fW
+#         )
+#         fW.write('\n')
+# subprocess.call(['gupload', f'blink_test_segmented/final_preds_{METHOD}.json'])
+#
+# for k, pars in cases_par.items():
+#     with open(f'blink_test_segmented/blink_summary_{k.replace("blink/", "")}_{METHOD}.json', mode='w') as fW:
+#         for ent in pars:
+#             json.dump(ent, fW)
+#             fW.write('\n')
+#     subprocess.call(['gupload', f'blink_test_segmented/blink_summary_{k.replace("blink/", "")}_{METHOD}.json'])
+
+
+############################################################################
+
 import json
 import subprocess
+
+
+
+
+
+
+all_segmented_ids = []
+all_paragraphs = []
+with open(f'blink_test_segmented/test_50_m1_all.json') as fR:
+    for l in fR:
+        all_segmented_ids.append(json.loads(l.strip())['id'])
+        all_paragraphs.append(json.loads(l.strip())['document'])
+
 
 segmented_preds = []
 with open('/trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/saved_models/bart-ext/bart-curr/generated_predictions.txt') as fR:
     for l in fR:
         segmented_preds.append(l.strip())
 
-
-METHOD = '300'
+METHOD = '50_m1_bart'
 segmented_ids = []
-paragraphs = []
+paragraphs_bart = []
 with open(f'blink_test_segmented/test_{METHOD}.json') as fR:
     for l in fR:
         segmented_ids.append(json.loads(l.strip())['id'])
-        paragraphs.append(json.loads(l.strip())['document'])
+        paragraphs_bart.append(json.loads(l.strip())['document'])
 
 cases = {}
 cases_par = {}
-for seg_id, par, seg_pred in zip(segmented_ids, paragraphs, segmented_preds):
+for seg_id, par, seg_pred in zip(segmented_ids, paragraphs_bart, segmented_preds):
     case_id = seg_id.split('-')[0]
     if case_id not in cases.keys():
         cases[case_id] = seg_pred
@@ -27,6 +90,15 @@ for seg_id, par, seg_pred in zip(segmented_ids, paragraphs, segmented_preds):
         cases[case_id] += seg_pred
         cases_par[case_id].append({'segment_id': seg_id, 'paragraph_text': par, 'paragraph_summary': seg_pred})
 
+
+cases_all = {}
+
+for seg_id, par in zip(all_segmented_ids, all_paragraphs):
+    case_id = seg_id.split('-')[0]
+    if seg_id in [s['segment_id'] for s in cases_par[case_id]]:
+        cases_all[case_id].append({'segment_id': seg_id, 'paragraph_text': par, 'paragraph_summary': cases_par[case_id][cases_par[case_id].index(seg_id)]})
+    else:
+        cases_all[case_id].append({'segment_id': seg_id, 'paragraph_text': par, 'paragraph_summary': 'NA'})
 
 with open(f'blink_test_segmented/final_preds_{METHOD}.json', mode='w') as fW:
     for k, v in cases.items():
@@ -39,7 +111,7 @@ with open(f'blink_test_segmented/final_preds_{METHOD}.json', mode='w') as fW:
         fW.write('\n')
 subprocess.call(['gupload', f'blink_test_segmented/final_preds_{METHOD}.json'])
 
-for k, pars in cases_par.items():
+for k, pars in cases_all.items():
     with open(f'blink_test_segmented/blink_summary_{k.replace("blink/", "")}_{METHOD}.json', mode='w') as fW:
         for ent in pars:
             json.dump(ent, fW)
