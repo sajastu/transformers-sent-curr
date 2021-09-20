b030ee97647f53b864c57f4982d454912c00bc41
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-09-20 02:21:09 -0400

    init

--- set_path.sh
+++ set_path.sh
@@ -1,12 +1,13 @@
 
-user_space_tr=$(python disk.py)
-
-! ln -sf $user_space_tr /home/code-base/user_space/
-
-for f in /home/code-base/user_space/tr*
-do
-  export SAVE_MODEL_DIR="${f}"
-done
+#user_space_tr=$(python disk.py)
+#
+#! ln -sf $user_space_tr /home/code-base/user_space/
+#
+#for f in /home/code-base/user_space/tr*
+#do
+#  export SAVE_MODEL_DIR="${f}"
+#done
+SAVE_MODEL_DIR=/trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MODEL=bart-ext
