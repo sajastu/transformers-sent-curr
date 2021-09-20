21094149a75839114e9e487abe5f67a30d57640e
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2021-09-20 14:07:35 -0400

    init

--- set_path.sh
+++ set_path.sh
@@ -10,9 +10,9 @@
 SAVE_MODEL_DIR=/trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9d1d9b2de/
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export MODEL=bart-ext
+export MODEL=bart
 #export M_ID=bart-sentence
-export M_ID=bart-tldr4M-pretrained-superloss
+export M_ID=bart-tldr4M
 
 export DS_BASE_DIR=$SAVE_MODEL_DIR/webis-tldr/splits
 #export DS_BASE_DIR=/tmp/transformers/reddit_tifu
