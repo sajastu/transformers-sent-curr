1ad4667d4d3f05a8f57ee226492af778dd1d51af
sajad <sajastu94_github>
2022-01-22 05:09:39 -0500

    update

--- set_path.sh
+++ set_path.sh
@@ -13,7 +13,7 @@ SAVE_MODEL_DIR=/trainman-mount/trainman-k8s-storage-349d2c46-5192-4e7b-8567-ada9
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MODEL=bart
 #export M_ID=bart-sentence
-export M_ID=bart-tldr4M
+export M_ID=bart-cnn
 
 export DS_BASE_DIR=$SAVE_MODEL_DIR/webis-tldr/splits
 #export DS_BASE_DIR=/tmp/transformers/reddit_tifu
