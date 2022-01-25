6c56a6ee3854905bd8ce7da1fbe0d733d3fa879b
sajad <sajastu94_github>
2022-01-25 00:37:36 -0500

    update

--- compute_rouge_generated.py
+++ compute_rouge_generated.py
@@ -1,4 +1,8 @@
 from datasets import load_dataset
+from spacy.lang.en import English
+nlp = English()
+nlp.add_pipe("sentencizer")
+
 
 save_path = '/saved_models/bart/bart-cnn/generated_predictions.txt'
 
@@ -10,8 +14,18 @@ test_dataset = raw_datasets['test']
 references = []
 
 for instance in test_dataset:
-    import pdb;pdb.set_trace()
+    references.append(instance['highlights'])
+
+output_lns = []
+for x in open(save_path).readlines():
+    output_sents = nlp(x.rstrip())
+    output_sents_text = []
+
+    for sent in output_sents:
+        import pdb;pdb.set_trace()
+        output_sents_text.append(sent.text)
 
 
-output_lns = [" . \n".join(x.rstrip().split('. ')) for x in open(save_path).readlines()]
+
+ # = [" . \n".join(.split('. ')) ]
 reference_lns = [" . \n".join(x.rstrip().split(' . ')) for x in open(args.reference_path).readlines()][: len(output_lns)]
