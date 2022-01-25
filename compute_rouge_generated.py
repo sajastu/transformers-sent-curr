8e197be3679e94efb9bde24e7b1f77f4636ed0bb
sajad <sajastu94_github>
2022-01-25 00:46:33 -0500

    update

--- compute_rouge_generated.py
+++ compute_rouge_generated.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 from datasets import load_metric
 from rouge_score import rouge_scorer, scoring
-from utils import calculate_bleu, calculate_rouge, chunks, parse_numeric_n_bool_cl_kwargs, use_task_specific_params
+from utils import calculate_rouge, parse_numeric_n_bool_cl_kwargs, use_task_specific_params
 
 metric = load_metric("rouge")
 
