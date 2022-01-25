9a37d9d58514d762a6f22b6d3b3166bb5c2175f6
sajad <sajastu94_github>
2022-01-25 13:17:36 -0500

    update

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -334,12 +334,12 @@ def main():
     # download the dataset.
     if data_args.dataset_name is not None:
         # Downloading and loading a dataset from the hub.
-        # raw_datasets = load_dataset(
-        #     data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir
-        # )
         raw_datasets = load_dataset(
-            'ccdv/cnn_dailymail', data_args.dataset_config_name, cache_dir=model_args.cache_dir
+            data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir
         )
+        # raw_datasets = load_dataset(
+        #     'ccdv/cnn_dailymail', data_args.dataset_config_name, cache_dir=model_args.cache_dir
+        # )
 
     else:
         data_files = {}
